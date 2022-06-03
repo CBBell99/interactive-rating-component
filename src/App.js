9b0058dab265ee956a0123a0578c43200cd78c21
CBBell99 <chrisbeckertonbell@gmail.com>
2022-06-03 04:13:33 -0700

    add text to page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>How did we do?</h2>
+      <p>
+        Please let us know how we did with your support request, All feedback is
+        appreciated to help us improve our offering!
+      </p>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </ul>
+      <button>Submit</button>
+    </div>
+  );
 }
 
 export default App;
