0b40a396c621db17726cddb52cde271884ccd53f
CBBell99 <chrisbeckertonbell@gmail.com>
2022-11-23 13:49:06 -0800

    basic skeleton of app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
+import star from "./images/icon-star.svg";
+
 function App() {
   return (
-    <div className="App">
-      <h2>How did we do?</h2>
-      <p>
-        Please let us know how we did with your support request, All feedback is
-        appreciated to help us improve our offering!
-      </p>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-      </ul>
-      <button>Submit</button>
-    </div>
+    <>
+      <div className="container">
+        <img src={star} alt="" />
+        <h2>How did we do?</h2>
+        <p>
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+        <ul>
+          <li>
+            <button>1</button>
+          </li>
+          <li>
+            <button>2</button>
+          </li>
+          <li>
+            <button>3</button>
+          </li>
+          <li>
+            <button>4</button>
+          </li>
+          <li>
+            <button>5</button>
+          </li>
+        </ul>
+        <button className="btn-rating">Submit</button>
+      </div>
+    </>
   );
 }
 
