e7f03fee62247d2cbc78cd4329df14e2a2f3f6ac
CBBell99 <chrisbeckertonbell@gmail.com>
2022-12-04 23:03:24 -0800

    reorg

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
 import star from "./images/icon-star.svg";
+import thanks from "./images/illustration-thank-you.svg";
 
 function App() {
   return (
     <>
-      <div className="container">
-        <div className="star-image">
-          <img src={star} alt="" />
-        </div>
-        <h2>How did we do?</h2>
+      <div className="card">
+        <img src={star} alt="star" />
+        <h1>How did we do?</h1>
         <p>
           Please let us know how we did with your support request. All feedback
           is appreciated to help us improve our offering!
         </p>
-        <ul className="rating">
+        <ul>
           <li>
-            <button>1</button>
+            <button className="btn">1</button>
           </li>
           <li>
-            <button>2</button>
+            <button className="btn">2</button>
           </li>
           <li>
-            <button>3</button>
+            <button className="btn">3</button>
           </li>
           <li>
-            <button>4</button>
+            <button className="btn">4</button>
           </li>
           <li>
-            <button>5</button>
+            <button className="btn">5</button>
           </li>
         </ul>
-        <button className="btn-rating">Submit</button>
+        <button className="btn-submit">SUBMIT</button>
+      </div>
+      <div className="card">
+        <div className="thank-you">
+          <img src={thanks} alt="POS machine" />
+        </div>
+        <p>You selected 4 out of 5</p>
+        <h2>Thank you!</h2>
+        <p>
+          We appreciate you taking your time to give a rating. If you ever need
+          more support, don't hesitate to get in touch!
+        </p>
       </div>
     </>
   );
